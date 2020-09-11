03b9a935dfaa4dda20b5935b122f0a49fa0dfad7
srinivas <srinivas@testgmail.com>
2020-09-11 16:40:31 +0100

    array learn update

---
+++ src/test/ArrayLearn.java
@@ -0,0 +1,15 @@
+package test;
+
+public class ArrayLearn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//int, float, double,... 
+		int[] arr = {10,20,30,40,50};
+		
+		int[] arr2 = new int[5];
+		arr2[0]=10;
+		
+	}
+}
